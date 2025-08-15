fc26ed19a9d7b53b52e1540460ae8397b4eaba3a
Vaibhav Mishra <vaibhavmishra805@gmail.com>
2025-08-15 18:56:39 +0530

    Added change_password and delete_profile features

--- routers/users.py
+++ routers/users.py
@@ -1,13 +1,19 @@
 
 from fastapi import APIRouter, HTTPException
+from pydantic import BaseModel, Field
 from starlette import status
 from dependencies import db_dependency, user_dependency
 from models import User
+from security import verify_password, hash_password
 
 router = APIRouter(
     prefix='/user',
     tags=['user']
 )
+
+class UserVerification(BaseModel):
+    old_password : str
+    new_password : str = Field(min_length = 8)
 
 @router.get('/me', status_code=status.HTTP_200_OK)
 async def get_user(user:user_dependency):
@@ -15,8 +21,25 @@ async def get_user(user:user_dependency):
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail='Authentication Failed')
     return user
 
-# @router.put('/password', status_code=)
-# async def change_password():
+@router.put('/password', status_code=status.HTTP_201_CREATED)
+async def change_password(passwordUpdateRequest : UserVerification,
+                          db : db_dependency, user : user_dependency):
+    if user is None:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail='Invalid User')
+    user_model = db.query(User).filter_by(id=user.get('id')).first()
+    if verify_password(passwordUpdateRequest.old_password, user_model.hashed_password):
+        user_model.hashed_password = hash_password(passwordUpdateRequest.new_password)
+    db.add(user_model)
+    db.commit()
+    return {'message' : 'Password Updated Successfully'}
 
-
-
+@router.delete('/delete_profile', status_code=status.HTTP_201_CREATED)
+async def delete_profile(user : user_dependency, db : db_dependency,
+                         userPassword : str):
+    if user is None:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail = 'Invalid User')
+    user_model = db.query(User).filter_by(id=user.get('id')).first()
+    if verify_password(userPassword, user_model.hashed_password):
+        db.delete(user_model)
+    db.commit()
+    return {'message' : 'User Deleted'}
