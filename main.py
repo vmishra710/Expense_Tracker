340af1040c7c8ef14a662f13fc3eecf5b1714de6
Vaibhav Mishra <vaibhavmishra805@gmail.com>
2025-09-05 01:03:27 +0530

    Redefined database.py for deployment

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.responses import RedirectResponse
 import models
 from database import Engine, Base
 from routers import auth, users, expenses
@@ -6,6 +7,10 @@ from routers import auth, users, expenses
 app = FastAPI(title='Expense Tracker API')
 
 models.Base.metadata.create_all(bind=Engine)
+
+@app.get('/', include_in_schema=False)
+def root():
+    return RedirectResponse(url='/docs')
 
 app.include_router(auth.router)
 app.include_router(users.router)
