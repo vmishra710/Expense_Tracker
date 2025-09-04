a5d0ee96ac32089495c4e2f96fc4961477481a8b
Vaibhav Mishra <vaibhavmishra805@gmail.com>
2025-09-05 00:54:15 +0530

    Redefined database.py for deployment

--- database.py
+++ database.py
@@ -1,9 +1,16 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 
-SQLALCHEMY_DATABASE_URL ='postgresql://postgres:Admin%401234@localhost/ExpenseTrackerDatabase'
+SQLALCHEMY_DATABASE_URL =os.getenv(
+    "DATABASE_URL", #Render will inject this
+    "postgresql://postgres:Admin%401234@localhost/ExpenseTrackerDatabase" #local fallback
+)
+
+if SQLALCHEMY_DATABASE_URL.startswith("postgres://"):
+    SQLALCHEMY_DATABASE_URL = SQLALCHEMY_DATABASE_URL.replace("postgres://", "postgresql://", 1)
 
 Engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
