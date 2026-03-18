27d207a53d18a90e2c43f7ebcff2757aec7370d7
Vaibhav Mishra <VaibhavMishra805@gmail.com>
2026-03-19 00:02:05 +0530

    Fixed Redis and DB config for production environment

--- celery_app.py
+++ celery_app.py
@@ -1,7 +1,7 @@
 from celery import Celery
 import os
 
-REDIS_URL = os.getenv('REDIS_URL', 'redis://localhost:6379')
+REDIS_URL = os.getenv('REDIS_URL')
 
 celery_app = Celery(
     'expense_tracker',
--- database.py
+++ database.py
@@ -3,6 +3,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
 
 
 SQLALCHEMY_DATABASE_URL =os.getenv("DATABASE_URL")
